b04941fe9472de31714b9f8c08ac7c4933676ba8
Samer Elhamdo <samerelhamdo@gmail.com>
2025-10-24 13:13:53 +0300

    Enhance admin API with additional database functionalities
    - Added F, Case, When, and DecimalField imports to support advanced query capabilities in the admin API.
    - Improved data handling for financial calculations and conditions, enhancing overall functionality.

--- app/admin_api.py
+++ app/admin_api.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.db import transaction
-from django.db.models import Count, Q, Sum, Max
+from django.db.models import Count, Q, Sum, Max, F, Case, When, DecimalField
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate
